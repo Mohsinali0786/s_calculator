f165bb4278feb86492d0c3b9e498a79d1e6035a7
mohsinali-iot <mohsinali481997@gmail.com>
2019-10-19 18:32:02 +0500

    first commit

---
+++ generator/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn cal(num1:u8,num2:u8)->u8
+{
+    let num1=5;
+    let num2=3;
+    let num3=num1+num2;
+    num3
+}
---
+++ generator/target/debug/libgenerator.d
@@ -0,0 +1,1 @@
+/home/mohsin/s_calculator/generator/target/debug/libgenerator.rlib: /home/mohsin/s_calculator/generator/src/lib.rs
