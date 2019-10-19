3be7f69fc22c246983568945a9a1aaef76acd35d
mohsinali-iot <mohsinali481997@gmail.com>
2019-10-19 19:48:57 +0500

    third commit

---
+++ generator/target/package/generator-0.1.0/target/debug/deps/generator-074e6017c6715b85.d
@@ -0,0 +1,7 @@
+/home/mohsin/s_calculator/generator/target/package/generator-0.1.0/target/debug/deps/generator-074e6017c6715b85.rmeta: src/lib.rs
+
+/home/mohsin/s_calculator/generator/target/package/generator-0.1.0/target/debug/deps/libgenerator-074e6017c6715b85.rlib: src/lib.rs
+
+/home/mohsin/s_calculator/generator/target/package/generator-0.1.0/target/debug/deps/generator-074e6017c6715b85.d: src/lib.rs
+
+src/lib.rs:
---
+++ generator/target/package/generator-0.1.0/target/debug/libgenerator.d
@@ -0,0 +1,1 @@
+/home/mohsin/s_calculator/generator/target/package/generator-0.1.0/target/debug/libgenerator.rlib: /home/mohsin/s_calculator/generator/target/package/generator-0.1.0/src/lib.rs
